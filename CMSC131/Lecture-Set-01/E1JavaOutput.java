3b1eea3e5fb5be09688a3b3bc846d10e8ab72067
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2015-04-17 17:05:58 -0700

    Java Output

---
+++ CMSC131/Lecture-Set-01/E1JavaOutput.java
@@ -0,0 +1,12 @@
+/ * This is a very basic Java program to get things started.
+  * Notice the difference between println and print
+  * The things inside the quotation marks are called "String
+  * literals" */
+ 
+public class E1JavaOutput {
+  public static void main (String args[]) {     // Where the program starts
+    System.out.println("Hello World!");
+    System.out.print("Or maybe I should say: ");
+    System.out.println("Goodbye World!");
+  }
+}
