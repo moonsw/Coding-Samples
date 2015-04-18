210b289885a08dc882a584db8ce64dc9d74f69a0
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2015-04-17 23:20:27 -0700

    For Loops vs. While Loops

---
+++ CMSC131/Lecture-Set-02/Loop-Examples/E6ForWhileCompare.java
@@ -0,0 +1,41 @@
+package LoopExamples;
+
+public class E6ForWhileCompare {
+  
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    final int START = 1;
+    final int END = 10;
+    
+    // Counter while loop examples.
+    int curr = START;
+    while (curr <= END) {
+      int i = 5;
+      System.out.println("counting at: " + curr + i);
+      curr = curr + 1;
+    }
+    System.out.println("I'm done; curr = " + curr);
+    
+    // For loop examples.
+    for (int cur = START; cur <= END; cur = cur + 1) {
+      System.out.println("counting at: " + cur);
+    }
+    
+    // Uncomment the next line to see ther error
+    // System.out.println("I'm done; cur = " + cur);
+    
+    for (int cur = END; cur >= START; cur = cur - 1) {
+      System.out.println("counting at: " + cur);
+    }
+    
+    for (int cur = 1024; cur >= 1; cur = cur / 2) {
+      System.out.println("counting at: " + cur);
+    }
+    
+    for (int cur = 1024; cur >= 1; cur = cur - 2) {
+      System.out.println("counting at: " + cur);
+    }
+  }
+}
