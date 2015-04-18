2145c94c36e90aa46aed982010a4ffe83e04be85
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2015-04-17 17:21:35 -0700

    Type Problems

---
+++ CMSC131/Lecture-Set-01/E5TypeProblems.java
@@ -0,0 +1,22 @@
+/* Demo of trying to combine types.
+ * Some are OK, others are not!!
+ */
+
+public class E5TypeProblems {
+  public static void main(String[] args) {
+    int x = 7; y = 12;
+    double d = 72.33;
+    boolean b = true;
+    char c;
+    String s;
+    
+    // Uncomment lines to see where there are problems
+    // x = y + 24;
+    // y = 17.3;
+    // d = x;
+    // b = 17;
+    // c = "cow";
+    // s = "Here is something weird " + x + y;
+    // System.out.println(s);
+  }
+}
