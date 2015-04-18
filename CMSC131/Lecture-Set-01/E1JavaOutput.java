df279a2a2aa396f7c82b53bbb942ec208aae86c5
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2015-04-17 17:06:35 -0700

    Java Output

--- CMSC131/Lecture-Set-01/E1JavaOutput.java
+++ CMSC131/Lecture-Set-01/E1JavaOutput.java
@@ -1,7 +1,7 @@
-/ * This is a very basic Java program to get things started.
-  * Notice the difference between println and print
-  * The things inside the quotation marks are called "String
-  * literals" */
+/* This is a very basic Java program to get things started.
+ * Notice the difference between println and print
+ * The things inside the quotation marks are called "String
+ * literals" */
  
 public class E1JavaOutput {
   public static void main (String args[]) {     // Where the program starts
