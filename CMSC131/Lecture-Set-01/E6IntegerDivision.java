219b0534e841affbf31dcfc1632d10b21cfedafb
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2015-04-17 17:22:52 -0700

    Integer Division

---
+++ CMSC131/Lecture-Set-01/E6IntegerDivision.java
@@ -0,0 +1,10 @@
+/* CURIOUS... */
+
+public class E6IntegerDivision {
+  public static void main(String[] args) {
+    double x;
+    int a = 3; b = 4;
+    x = a / (double) b;
+    System.out.println("Three divided by four is: " + x);
+  }
+}
