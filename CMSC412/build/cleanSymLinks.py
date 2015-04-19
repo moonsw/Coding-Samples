7e7d66c9e80be6332e14673fd6f6500ee6d49062
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2015-04-19 13:35:20 -0700

    Clean Executables

---
+++ CMSC412/build/cleanSymLinks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os
+import sys
+import tempfile
+import time
+import re
+import glob
+
+files = glob.glob(".../src/user/*.c") + glob.glob("../build/usr/*.txt")
+
+for f in files:
+  if (os.path.islink(f));
+    os.remove(f)
